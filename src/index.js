33a1e18431ce6718340b1bb5436a5a4811fe3e4e
Jacob Grahn <jacob@grahn.io>
2020-09-01 00:25:35 -0400

    add a router and body-parser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const Koa = require('koa')
 const Redis = require('redis')
+const bodyParser = require('koa-bodyparser')
+const Router = require('@koa/router')
+
 const app = new Koa()
 const redis = Redis.createClient('redis://redis')
- 
-// response
-app.use(ctx => {
+const router = new Router()
+
+// routes
+router.get('/', (ctx, next) => {
   ctx.body = 'Hello Koa'
 })
- 
+
+router.get('/test', (ctx) => {
+  ctx.body = 'This is a test'
+})
+
+// parse incoming json requests
+app.use(bodyParser({enableTypes: ['json']}))
+
+// use the router
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+// hey! listen! (it's me, navi)
 app.listen(8080)
 
 
